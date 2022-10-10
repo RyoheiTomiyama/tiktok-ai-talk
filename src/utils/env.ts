1841760ba99cbb4734ff149c9902f12f23fe2406
富山涼平 <tennisnowboarderyou@gmail.com>
2022-10-11 00:03:23 +0900

    add env設定

---
+++ src/utils/env.ts
@@ -0,0 +1,33 @@
+// .envの型を定義
+const EnvType = {
+    NODE_ENV: 'string',
+    VOICEVOX_API_URL: 'string',
+    NUM: 'number',
+} as const
+
+type EnvKey = keyof typeof EnvType
+type Result<T extends EnvKey> = typeof EnvType[T] extends 'string' ? string
+    : typeof EnvType[T] extends 'number' ? number : boolean
+
+export const Env = {
+    get<T extends EnvKey>(name: T): Result<T> {
+        const value = process.env[name]
+        if (!value) {
+            throw Error(`Environment: ${name} is not found.`)
+        }
+        switch (EnvType[name]) {
+            case 'string': {
+                return <Result<T>>value
+            }
+            case 'number': {
+                return <Result<T>>Number(value)
+            }
+            case 'boolean': {
+                return <Result<T>>Boolean(value)
+            }
+            default: {
+                throw Error(`Environment: ${name} is unknown type.`)
+            }
+        }
+    },
+}
