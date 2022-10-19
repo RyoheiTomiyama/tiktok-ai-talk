5716babd7eceab244f66983e63f8685e1a2fcee5
富山涼平 <tennisnowboarderyou@gmail.com>
2022-10-19 11:33:45 +0900

    add Giftの画像ダウンロードできるようにしておく

--- packages/client/src/components/TiktokLive.tsx
+++ packages/client/src/components/TiktokLive.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react'
 import { io } from 'socket.io-client'
-import { Env } from '@/utils/env';
+import { Env } from '@/utils/env'
 
 const socket =io(Env.get('SOCKET_SERVER'), {
   // withCredentials: true,
-  query: {
-    // username: 'joemsaaaaa',
-    username: 'ebichan813',
-  },
+  // query: {
+  //   // username: 'joemsaaaaa',
+  //   username: 'ebichan813',
+  // },
 })
 
 const TiktokLive: React.FC = () => {
   const username = 'joemsaaaaa'
+  // const username = 'sekia2518'
   useEffect(() => {
     socket.emit('enter', { username })
     socket.on('connect_error', (err) => {
@@ -22,12 +23,31 @@ const TiktokLive: React.FC = () => {
       socket.disconnect()
     })
     socket.on('chat', (data) => {
+      // console.log(data)
+    })
+    socket.on('like', (data) => {
       console.log(data)
+    })
+    socket.on('gift', async (data) => {
+      console.log(data)
+      if (document && data.diamondCount > 30) {
+        const button = document.createElement('a')
+
+        const image = await fetch(data.giftPictureUrl);
+        const imageBlob = await image.blob();
+        const imageURL = URL.createObjectURL(imageBlob);
+
+        button.download = `${data.giftName}_${data.diamondCount}.png`
+        button.href = imageURL
+        button.click()
+      }
     })
     return () => {
       socket.off('connect_error')
       socket.off('disconnect')
       socket.off('chat')
+      socket.off('like')
+      socket.off('gift')
     }
   }, [])
   return null
